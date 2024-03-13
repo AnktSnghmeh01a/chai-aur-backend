d6f9f0f74a215f3a6a82cbeba6faa12089e1e7b4
AnktSnghmeh01a <ankitsinghmehra@12345678@gmail.com>
2024-03-13 19:32:51 +0530

    update the spellling mistake

--- src/utils/ApiErrors.js
+++ src/utils/ApiErrors.js
@@ -4,7 +4,7 @@ class ApiError extends Error {
     statusCode,
     message="Something went wrong",
     error = [],
-    statck = ""
+    stack = ""
   ){
     super(message)
     this.statusCode = statusCode
@@ -13,8 +13,8 @@ class ApiError extends Error {
     this.success = false
     this.errors = error
     
-    if(statck){
-   this.stack = statck;
+    if(stack){
+   this.stack = stack;
   } 
   else{
     Error.captureStackTrace(this, this.constructor)
